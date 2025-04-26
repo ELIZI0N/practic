cada1d9d1f7b03f5065a5235e9314e70359dc9b6
el <grigori040903@gmail.com>
2025-04-26 12:11:53 +0300

    убрано окно аккаунта

--- app/src/main/java/com/example/practic/MainActivity.kt
+++ app/src/main/java/com/example/practic/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_manga, R.id.navigation_search, R.id.navigation_accaunt
+                R.id.navigation_home, R.id.navigation_manga, R.id.navigation_search
             )
         )
         navView.setupWithNavController(navController)
